0b049bcde3eff421557f47ff16388bcd3ba43794
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-12-11 17:54:32 -0300

    create_payment/3 function and it's helpers

--- lib/junex/client.ex
+++ lib/junex/client.ex
@@ -27,7 +27,31 @@ defmodule Junex.Client do
           amount: float(),
           paymentTypes: String.t()
         }
-  @type billing :: %{name: String.t(), document: String.t(), email: String.t(), phone: String.t()}
+  @type charge_billing_info :: %{
+          name: String.t(),
+          document: String.t(),
+          email: String.t(),
+          phone: String.t()
+        }
+  @type payment_billing_info :: %{
+          email: String.t(),
+          address: %{
+            street: String.t(),
+            number: integer(),
+            complement: String.t(),
+            city: String.t(),
+            state: String.t(),
+            postCode: String.t()
+          }
+        }
+  @type card_info :: %{
+          creditCardHash: String.t()
+        }
+  @type payment_info :: %{
+          chargeId: String.t(),
+          billing: payment_billing_info(),
+          creditCardDetails: card_info()
+        }
 
   @doc """
   Returns a new charge to be used on Junex.Client.create_charge/2
@@ -84,7 +108,7 @@ defmodule Junex.Client do
   @doc """
   Return a new billing map to be used on Junex.Client.create_charge/2
   """
-  @spec get_billing(String.t(), String.t(), String.t(), String.t()) :: billing()
+  @spec get_billing(String.t(), String.t(), String.t(), String.t()) :: charge_billing_info()
   def get_billing(name, doc, email, phone)
       when is_binary(name) and is_binary(doc) and is_binary(email) and is_binary(phone) do
     %{
@@ -107,7 +131,7 @@ defmodule Junex.Client do
   @spec create_charge(
           %Tesla.Client{},
           total_charge_info() | charge_info(),
-          billing(),
+          charge_billing_info(),
           atom()
         ) ::
           {:ok, map()} | {:error, atom() | String.t() | {atom(), atom()}}
@@ -146,6 +170,88 @@ defmodule Junex.Client do
           get_conn_error(error)
       end
       |> JSON.encode(keys: :string)
+
+    check_status_code(status, body)
+  end
+
+  @doc """
+  Return a card_info map to use on Junex.Client.get_payment_info/2
+  """
+  @spec get_card_info(String.t()) :: card_info()
+  def get_card_info(card_hash) when is_binary(card_hash) do
+    %{
+      creditCardHash: card_hash
+    }
+  end
+
+  @doc """
+  Returns a payment_billing_info map to use on Junex.Client.get_payment_info/2
+  """
+  @spec get_payment_billing_info(
+          String.t(),
+          String.t(),
+          integer(),
+          String.t(),
+          String.t(),
+          String.t(),
+          String.t()
+        ) :: payment_billing_info()
+  def get_payment_billing_info(email, street, st_number, compl, city, state, post_code)
+      when is_binary(email) and is_binary(street) and is_binary(st_number) and is_binary(compl) and
+             is_binary(city) and is_binary(state) and is_binary(post_code) do
+    %{
+      email: email,
+      address: %{
+        street: street,
+        number: st_number,
+        complement: compl,
+        city: city,
+        state: state,
+        postCode: post_code
+      }
+    }
+  end
+
+  @doc """
+  Returns a payment_info map to be used on Junex.Client.create_payment/3
+
+  ## Parameters
+    - charge_id: Result of one entries of Junex.Client.create_charge/4
+    - card_info: Build mannualy or got from Junex.Client.get_card_info/1
+    - payment_billing_info: Build mannually or got from Junex.Client.get_payment_billing_info/7
+  """
+  @spec get_payment_info(String.t(), card_info(), payment_billing_info()) :: payment_info()
+  def get_payment_info(charge_id, card_info, payment_billing_info) do
+    %{
+      chargeId: charge_id,
+      billing: payment_billing_info,
+      creditCardDetails: card_info
+    }
+  end
+
+  @doc """
+  Creates and returns a new Payment
+
+  ## Parameters
+    - client: Got from Junex.Client.create/2
+    - payment_info: Build mannualy or got from Junex.Client.get_payment_info/3
+    - mode: :prod | :sandbox
+  """
+  @spec create_payment(%Tesla.Client{}, payment_info(), atom()) ::
+          {:ok, map()} | {:error, atom() | String.t() | {atom(), atom()}}
+  def create_payment(%Tesla.Client{} = client, payment_info, mode)
+      when is_map(payment_info) and mode in @modes do
+    {:ok, %{status: status, body: body}} =
+      case post(client, get_url(mode) <> "/payments", payment_info) do
+        {:ok, env} ->
+          env
+
+        {:error, error} ->
+          get_conn_error(error)
+      end
+      |> JSON.decode(keys: :string)
+
+    check_status_code(status, body)
   end
 
   @doc """
@@ -162,7 +268,7 @@ defmodule Junex.Client do
   @spec list_banks(Tesla.client(), atom()) ::
           {:ok, list(map())}
           | {:error, atom() | String.t() | {atom(), atom()}}
-  def list_banks(%Tesla.Client{} = client, mode) when is_atom(mode) and mode in @modes do
+  def list_banks(%Tesla.Client{} = client, mode) when mode in @modes do
     {:ok, %{status: status, body: body}} =
       case get(client, get_url(mode) <> "/data/banks", []) do
         {:ok, env} ->
@@ -177,7 +283,7 @@ defmodule Junex.Client do
     check_status_code(status, body, "_embedded", "banks")
   end
 
-  def list_banks(%Tesla.Client{} = _client, mode) when not is_atom(mode) or mode not in @modes,
+  def list_banks(%Tesla.Client{} = _client, mode) when mode not in @modes,
     do: get_atom_error()
 
   def list_banks(_invalid, _mode), do: get_client_error()
@@ -194,7 +300,7 @@ defmodule Junex.Client do
     {:ok, %{"links" => _, "balance" => _, "transferableBalance" => _, "withheldBalance" => _}}
   """
   @spec get_balance(%Tesla.Client{}, atom()) :: {:ok, map()} | {:error, atom()}
-  def get_balance(%Tesla.Client{} = client, mode) when is_atom(mode) and mode in @modes do
+  def get_balance(%Tesla.Client{} = client, mode) when mode in @modes do
     {:ok, %{status: status, body: body}} =
       case get(client, get_url(mode) <> "/balance", []) do
         {:ok, env} ->
@@ -208,7 +314,7 @@ defmodule Junex.Client do
     check_status_code(status, body)
   end
 
-  def get_balance(%Tesla.Client{} = _client, mode) when not is_atom(mode) or mode not in @modes,
+  def get_balance(%Tesla.Client{} = _client, mode) when mode not in @modes,
     do: get_atom_error()
 
   def get_balance(_invalid, _mode), do: get_client_error()
