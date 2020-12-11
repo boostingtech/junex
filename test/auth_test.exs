0079ea44028cc4a12442113a89e922bfff3790cd
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2020-12-11 01:36:23 -0300

    tests passing

--- test/auth_test.exs
+++ test/auth_test.exs
@@ -22,5 +22,31 @@ defmodule Junex.AuthTest do
       assert {:error, {:unauthenticated, :wrong_credentials}} =
                Auth.get_access_token(client_id, client_secret, is_sandbox)
     end
+
+    test "returns error when is_sandbox is not a boolean" do
+      client_id = "teste1"
+      client_secret = "teste2"
+
+      Mox.expect(Junex.AuthMock, :get_access_token, 1, fn ^client_id, ^client_secret, "test" ->
+        {:error, :expected_boolean}
+      end)
+
+      assert {:error, :expected_boolean} = Auth.get_access_token(client_id, client_secret, "test")
+    end
+
+    test "returns ok and token if data is correct" do
+      client_id = "teste1"
+      client_secret = "teste2"
+      is_sandbox = true
+
+      Mox.expect(Junex.AuthMock, :get_access_token, 1, fn ^client_id,
+                                                          ^client_secret,
+                                                          ^is_sandbox ->
+        {:ok, "access_token"}
+      end)
+
+      assert {:ok, access_token} = Auth.get_access_token(client_id, client_secret, is_sandbox)
+      assert is_binary(access_token)
+    end
   end
 end
